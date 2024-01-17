49c7cf42656d86262c6801d23d00741d7c1c12a1
I_am_Akash <144481400+akky2000@users.noreply.github.com>
2024-01-17 21:48:01 +0530

    index.js

--- todo-app/index.js
+++ todo-app/index.js
@@ -3,6 +3,7 @@
 
 
 const app = require("./app");
-app.listen(3000, () => {
-  console.log(`http://localhost:3000`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT,()=>{
+    console.log('started express server');
 });
