dc70828a073cd503e0afdbbd1b32b3b580134d92
I_am_Akash <144481400+akky2000@users.noreply.github.com>
2024-01-21 16:33:06 +0530

    Update app.js

--- todo-app/app.js
+++ todo-app/app.js
@@ -21,7 +21,7 @@ app.get("/", async (request, response) => {
    
 
     if (request.accepts("html")) {
-      response.render('index.ejs', 'todos.ejs'{
+      response.render('index.ejs',{
         allTodos,
          overdue: overdueTodos,
                duetoday:dueTodayTodos,
