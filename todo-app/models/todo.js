c78a367756d3f965432223ea75a618d700942ced
I_am_Akash <144481400+akky2000@users.noreply.github.com>
2024-01-21 13:19:59 +0530

    Update todo.js

--- todo-app/models/todo.js
+++ todo-app/models/todo.js
@@ -18,7 +18,7 @@ module.exports = (sequelize, DataTypes) => {
       return this.create({title: title, dueDate: dueDate, completed: false});
     }
 
-    static getTodos(){
+    static async getTodos(){
       return this.findAll();
     }
 
