4a4dff91f5df51d7a01d2f0f0697884953da1cd5
I_am_Akash <144481400+akky2000@users.noreply.github.com>
2024-01-23 21:14:54 +0530

    Update todos.js

--- todo-app/__tests__/todos.js
+++ todo-app/__tests__/todos.js
@@ -65,58 +65,6 @@ describe("Todo test suite", () => {
     expect(res.status).toBe(500);
   });
 
-  // test("Marking a sample overdue item as completed", async () => {
-  //   // Create an overdue todo
-  //   const overdueRes = await agent.post("/todos").send({
-  //     title: "Overdue Todo",
-  //     dueDate: "2015-11-01",
-  //     completed: false,
-  //   });
-
-  //   // Extract the ID of the created todo
-  //   console.log("overdueRes.header.location:", overdueRes.header.location);
-  //   const overdueTodoId = overdueRes.header.location ? Number(overdueRes.header.location.split("/")[2]) : null;
-
-  //   //const overdueTodoId = Number(overdueRes.header.location.split("/")[2]);
-
-  //   const markCompletedResponse = await agent.put(`/todos/${overdueTodoId}`).send({
-  //     _csrf: extractCsrfToken(overdueRes),
-  //     completed: true,
-  //   });
-  //   console.log("markCompletedResponse.body:", markCompletedResponse.body);
-
-  //   expect(markCompletedResponse.status).toBe(500);
-  //   expect(markCompletedResponse.body?.completed).toBe(true);
-  // });
-
-  // test("Toggle a completed item to incomplete when clicked on it", async () => {
-  //   const completedTodo = await agent.post("/todos").send({
-  //     title: "complete Todo",
-  //     dueDate: new Date().toISOString().split("T")[0],
-  //     completed: true,
-  //   });
-
-  //   console.log("completedTodo:", completedTodo);
-  //   let completedTodoId = null;
-  //   if (completedTodo && completedTodo.header && completedTodo.header.location) {
-  //     completedTodoId = Number(completedTodo.header.location.split("/")[2]);
-  //   }
-
-  //   // console.log("overdueRes.header.location:", completedTodo.header.location);
-  //   // const completedTodoId = completedTodo.header.location ? Number(completedTodo.header.location.split("/")[2]) : null;
-
-  //   //const completedTodoId = Number(completedTodo.header.location.split("/")[2]);
-
-  //   const toggleResponse = await agent.put(`/todos/${completedTodoId}`).send({
-  //     _csrf: extractCsrfToken(completedTodo),
-  //     completed: false,
-  //   });
-  //   console.log("toggleResponse.body:", toggleResponse.body);
-
-  //   expect(toggleResponse.status).toBe(500);
-  //   expect(toggleResponse.body?.completed).toBe(true);
-  // });
-
   test("Delete a todo item", async () => {
     const createTodo = await agent.post("/todos").send({
       title: "Todo to Delete",
