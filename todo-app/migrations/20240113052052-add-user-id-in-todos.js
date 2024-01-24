e9301dc4ee845207ba2ca913c0ae21c3de155792
I_am_Akash <144481400+akky2000@users.noreply.github.com>
2024-01-24 17:25:45 +0530

    20240113052052-add-user-id-in-todos.js

--- todo-app/migrations/20240113052052-add-user-id-in-todos.js
+++ todo-app/migrations/20240113052052-add-user-id-in-todos.js
@@ -2,34 +2,35 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-    async up(queryInterface, Sequelize) {
-        await queryInterface.addColumn('Todos', 'userId', {
-            type: Sequelize.DataTypes.INTEGER
-        })
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn('Todos', 'UserId', {
+      type: Sequelize.DataTypes.INTEGER,
 
-        await queryInterface.addConstraint('Todos', {
-            fields: ['userId'],
-            type: 'foreign key',
-            references: {
-                table: 'Users',
-                field: 'id'
-            }
-        })
-        /**
-         * Add altering commands here.
-         *
-         * Example:
-         * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-         */
-    },
+    })
+    await queryInterface.addConstraint('Todos', {
+      fields: ['UserId'],
+      type: 'foreign key',
+      references: {
+        table: 'Users',
+        field: 'id'
+      }
 
-    async down(queryInterface, Sequelize) {
-        await queryInterface.removeColumn('Todos', 'userId')
-        /**
-         * Add reverting commands here.
-         *
-         * Example:
-         * await queryInterface.dropTable('users');
-         */
-    }
+    })
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeColumn('Todos', 'UserId')
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
 };
