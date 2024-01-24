33d63a116eb4d094a7753799410bd46d9987beea
I_am_Akash <144481400+akky2000@users.noreply.github.com>
2024-01-24 16:37:35 +0530

    Update user.js

--- todo-app/models/user.js
+++ todo-app/models/user.js
@@ -1,47 +1,26 @@
-
-
-
-
 'use strict';
 const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-   
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
     static associate(models) {
-      // define association here
-      User.hasMany(models.Todo,{
+      User.hasMany(models.Todo, {
         foreignKey: 'userId'
       })
+      // define association here
     }
   }
   User.init({
-    firstName: {
-      type: DataTypes.STRING,
-      allowNull: false, 
-      validate: {
-          notNull: true,
-          len: 1
-      }
-   },
-    LastName: DataTypes.STRING,
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false, 
-      validate: {
-          notNull: true,
-          len: 1
-      }
-   },
-   password: {
-    type: DataTypes.STRING,
-    allowNull: false, 
-    validate: {
-        notNull: true,
-        len: 6
-    }
- },
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING
   }, {
     sequelize,
     modelName: 'User',
