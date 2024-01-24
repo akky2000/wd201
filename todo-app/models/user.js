6c6b24b80c7a2ea5c886c4e448b671c8e11394e5
I_am_Akash <144481400+akky2000@users.noreply.github.com>
2024-01-24 20:44:11 +0530

    Update user.js

--- todo-app/models/user.js
+++ todo-app/models/user.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       User.hasMany(models.Todo,{
-        foreignKey:'UserId',
+        foreignKey:'userId'
       })
       // define association here
     }
