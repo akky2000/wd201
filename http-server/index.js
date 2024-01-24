fe91dc893fb88b8aec15ceb4cd6c502430ed7138
I_am_Akash <144481400+akky2000@users.noreply.github.com>
2024-01-24 17:19:25 +0530

    index.js

--- http-server/index.js
+++ http-server/index.js
@@ -1,52 +1,60 @@
-const args = require("minimist")(process.argv.slice(2));
-
 const http = require("http");
 const fs = require("fs");
+const minimist = require("minimist");
 
-const port = 3000;
-
-let homeInfo = "";
-let projectInfo = "";
-let registrationInfo = "";
+let homeContent = "";
+let projectContent = "";
+let registrationContent = "";
 
 fs.readFile("home.html", (err, home) => {
-  if (err) {
-    throw err;
-  }
-  homeInfo = home;
+    if (err) {
+        throw err;
+    }
+    homeContent = home;
 });
 
 fs.readFile("project.html", (err, project) => {
-  if (err) {
-    throw err;
-  }
-  projectInfo = project;
-});
-
-fs.readFile("registration.html", (err, registration) => {
-  if (err) {
-    throw err;
-  }
-  registrationInfo = registration;
-});
-
-http
-  .createServer((request, response) => {
-    let url = request.url;
-    response.writeHeader(200, { "Content-Type": "text/html" });
-    switch (url) {
-      case "/project":
-        response.write(projectInfo);
-        response.end();
-        break;
-      case "/registration":
-        response.write(registrationInfo);
-        response.end();
-        break;
-      default:
-        response.write(homeInfo);
-        response.end();
-        break;
+    if (err) {
+        throw err;
     }
-  })
-  .listen(args.port);
+    projectContent = project;
+});
+
+fs.readFile("registration.html", (err, register) => {
+    if (err) {
+        throw err;
+    }
+    registrationContent = register;
+});
+
+const server = http.createServer((request, response) => {
+    let url = request.url;
+    response.writeHead(200, { "Content-Type": "text/html" });
+    switch (url) {
+        case "/project":
+            response.write(projectContent);
+            response.end();
+            break;
+        case "/registration":
+            response.write(registrationContent);
+            response.end();
+            break;
+        case "/home":
+            response.write(homeContent);
+            response.end();
+            break;
+        default:
+            response.write(homeContent);
+            response.end();
+            break;
+    }
+});
+
+const args = minimist(process.argv.slice(2));
+const port = args.port;
+
+server.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
+
+
