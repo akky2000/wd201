f2f4c37e5166fd7325ad96d70666a0ae28a91227
I_am_Akash <144481400+akky2000@users.noreply.github.com>
2024-01-24 20:41:09 +0530

    Update 20240113054022-create-todo.js

--- todo-app/migrations/20240113054022-create-todo.js
+++ todo-app/migrations/20240113054022-create-todo.js
@@ -1,43 +1,40 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Todos', {
+    await queryInterface.createTable("Todos", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       title: {
         type: Sequelize.STRING,
-        allowNull: false,
+        allowNull: false, 
         validate: {
-          notNull: true,
-
+            notNull: true,
+            len: 5
         }
       },
       dueDate: {
         type: Sequelize.DATEONLY,
-        allowNull: false,
-        validate: {
-          notNull: true
-        }
       },
       completed: {
-        type: Sequelize.BOOLEAN
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Todos');
-  }
+    await queryInterface.dropTable("Todos");
+  },
 };
