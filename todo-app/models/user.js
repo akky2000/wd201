37c32d0dcd90938de9419a57ef6b93df3de81bb8
I_am_Akash <144481400+akky2000@users.noreply.github.com>
2024-01-24 17:28:21 +0530

    user.js

--- todo-app/models/user.js
+++ todo-app/models/user.js
@@ -10,8 +10,8 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      User.hasMany(models.Todo, {
-        foreignKey: 'userId'
+      User.hasMany(models.Todo,{
+        foreignKey:'UserId',
       })
       // define association here
     }
