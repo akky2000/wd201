5d75ad36edfa8c1df0e6092f01e663d149b17e48
I_am_Akash <144481400+akky2000@users.noreply.github.com>
2024-01-29 15:20:08 +0530

    Update 20240116183029-create-user.js

--- todo-app/migrations/20240116183029-create-user.js
+++ todo-app/migrations/20240116183029-create-user.js
@@ -10,15 +10,16 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       firstName: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
       },
       lastName: {
         type: Sequelize.STRING
       },
       email: {
         type: Sequelize.STRING,
-        allowNull:false,
-        unique:true
+        allowNull: false,
+        unique: true
       },
       password: {
         type: Sequelize.STRING
