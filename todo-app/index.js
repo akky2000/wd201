b6984d761890cb630bbee2a33d8ae7965c5deaee
I_am_Akash <144481400+akky2000@users.noreply.github.com>
2024-01-29 23:22:50 +0530

    Update index.js

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,5 +1,5 @@
 const app = require('./app');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => {
-    console.log("started express server at port 3000")
+    console.log("started express server at port 5050")
 })
