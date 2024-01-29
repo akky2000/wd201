c455f358dc198e48c3480cb988ff8bd0db254309
I_am_Akash <144481400+akky2000@users.noreply.github.com>
2024-01-29 22:06:25 +0530

    Update app.js

--- todo-app/app.js
+++ todo-app/app.js
@@ -27,6 +27,8 @@ app.use(session({
   cookie: {
     maxAge: 24 * 60 * 60 * 1000,
   },
+	 resave: false,
+  saveUninitialized: false,
 }));
 app.use(passport.initialize());
 app.use(passport.session());
